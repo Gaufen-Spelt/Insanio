0a567f1170b79b2378c97de87f1e4ee4b2c5bbcb
Gaufen Johannes <kesselfarben@gmail.com>
2026-06-14 21:47:11 +0500

    f

--- out/html/game.js
+++ out/html/game.js
@@ -655,9 +655,8 @@ window.populateIntroOption = function() {
   var q = game && game.qualities;
   if (q) {
     q['tick'] = (q['tick'] || 0) + 1;
-    if (q['tick'] % 60 === 0) {
-      ui.dendryEngine.goToScene('post_event');
-    }
+    var el = document.querySelector('#tick-display');
+    if (el) el.textContent = q['tick'];
   }
 }, 1000);
 
